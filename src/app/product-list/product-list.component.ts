1784feae87d9a35bc4a04763bbb54d88454322b0
eldanialonso <eldani.alonso@gmail.com>
2020-12-03 23:09:42 -0300

    3 files modified, 3 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,20 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { products } from '../products';
+import { products } from "../products";
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent {
   products = products;
 
-  share() {
-    window.alert('The product has been shared!');
+  share(product) {
+    window.alert("The product " + product.name + " has been shared!");
+  }
+
+  onNotifyYeah(product) {
+    window.alert("You will be notified when " + product.name + " goes on sale");
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
